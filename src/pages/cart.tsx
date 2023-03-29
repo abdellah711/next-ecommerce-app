e1e9932d6a62b63866b45ea96f0ad363cdabf05c
abdellah711 <alaouiabdellah711@gmail.com>
2023-03-29 17:05:59 +0200

    save cart state in localstorage and handle hydration mismatch issue

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -3,10 +3,19 @@ import CartIcon from '@heroicons/react/24/solid/ShoppingCartIcon'
 import CartItem from "@/components/cart/CartItem"
 import CartSummary from "@/components/cart/CartSummary"
 import { useCartStore } from "@/store/cart"
+import { useEffect, useState } from 'react'
 
 
 const CartPage = () => {
     const items = useCartStore(state => state.cartItems)
+    const [isLoading, setIsLoading] = useState(true) // used to avoid hydration mismatch
+    
+    useEffect(() => {
+        setIsLoading(false)
+    }, [])
+
+    if (isLoading) return <div className='h-screen grid place-items-center'><div className='w-9 aspect-square rounded-full border-4 border-zinc-200 border-t-zinc-700 animate-spin -mt-[10vh]'/></div>
+
     return (
         <div className="content min-h-screen w-full">
             <h1 className="text-3xl py-7 text-zinc-700">
@@ -27,5 +36,6 @@ const CartPage = () => {
         </div>
     )
 }
+
 
 export default CartPage
--- src/store/cart.ts
+++ src/store/cart.ts
@@ -1,5 +1,6 @@
 import { Product } from "@/types/product";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type CartState = {
     cartItems: { product: Product, color?: string, size?: number, quantity: number }[];
@@ -8,33 +9,37 @@ type CartState = {
     deleteItem: (product: Product, color?: string, size?: number) => void;
 }
 
-export const useCartStore = create<CartState>((set) => ({
-    cartItems: [],
-    addToCart: (product: Product, quantity = 1, color?: string, size?: number) => set((state) => {
-        const cartItems = [...state.cartItems];
-        const item = cartItems.find((item) => item.product.id === product.id);
-        if (item && item.color === color && item.size === size) {
-            item.quantity = Math.min(item.quantity + quantity, product.attributes.stock);
-        } else {
-            cartItems.push({ product, quantity, color, size });
-        }
-        return { cartItems };
-    }),
-    changeQuantity: (product: Product, quantity: number, color?: string, size?: number) => set((state) => {
-        const cartItems = [...state.cartItems];
-        const item = cartItems.find((item) => item.product.id === product.id);
-        if (item && item.color === color && item.size === size) {
-            item.quantity = quantity;
-        }
-        return { cartItems };
-    }),
-    deleteItem: (product: Product, color?: string, size?: number) => set((state) => {
-        const cartItems = [...state.cartItems];
-        const item = cartItems.find((item) => item.product.id === product.id && item.color === color && item.size === size);
-        if (item) {
-            const index = cartItems.indexOf(item);
-            cartItems.splice(index, 1);
-        }
-        return { cartItems };
-    }),
-}))
+export const useCartStore = create(
+    persist<CartState>((set) => ({
+        cartItems: [],
+        addToCart: (product: Product, quantity = 1, color?: string, size?: number) => set((state) => {
+            const cartItems = [...state.cartItems];
+            const item = cartItems.find((item) => item.product.id === product.id);
+            if (item && item.color === color && item.size === size) {
+                item.quantity = Math.min(item.quantity + quantity, product.attributes.stock);
+            } else {
+                cartItems.push({ product, quantity, color, size });
+            }
+            return { cartItems };
+        }),
+        changeQuantity: (product: Product, quantity: number, color?: string, size?: number) => set((state) => {
+            const cartItems = [...state.cartItems];
+            const item = cartItems.find((item) => item.product.id === product.id);
+            if (item && item.color === color && item.size === size) {
+                item.quantity = quantity;
+            }
+            return { cartItems };
+        }),
+        deleteItem: (product: Product, color?: string, size?: number) => set((state) => {
+            const cartItems = [...state.cartItems];
+            const item = cartItems.find((item) => item.product.id === product.id && item.color === color && item.size === size);
+            if (item) {
+                const index = cartItems.indexOf(item);
+                cartItems.splice(index, 1);
+            }
+            return { cartItems };
+        }),
+    }), {
+        name: "cart"
+    })
+)
